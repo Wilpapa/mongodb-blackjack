0d77ec42abcb3aae304398ba4e95d7a3590097b9
Guillaume Meister <Wilpapa@users.noreply.github.com>
2019-03-25 23:45:36 +0100

    comments updates

--- mongo-blackjack.py
+++ mongo-blackjack.py
@@ -13,7 +13,7 @@ from classes import *
 
 MONGOURI="mongodb://localhost" #MongoDB connection string
 SAMPLES=1000 #number of games to play
-THRESHOLD=15 #score under which player will draw 1 card
+THRESHOLD=15 #score under which Player will draw 1 card
 NB_DECKS=4 #number of decks to use. Decks are reshuffled when half the cards have been drawn
 START_TIME=datetime.now()
 
@@ -30,24 +30,23 @@ for i in range(SAMPLES):
     
     if p_human.blackjack:
         if p_casino.blackjack:
-            p_human.gain=0 # casino and player both have a blackjack, it's a deuce
+            p_human.gain=0 # Casino and Player both have a blackjack, it's a deuce
         else:
-            p_human.gain=2 # player has a blackjack, double win
+            p_human.gain=2 # Player alone has a Blackjack, double win
     else:
-        while (p_human.score<THRESHOLD): # player will draw cards until reaching THRESHOLD score
+        while (p_human.score<THRESHOLD): # Player will draw cards until reaching THRESHOLD score
             p_human.draw_card(c_deck)
         if (p_human.score<=21):
-            while (p_casino.score<17): # by casion rule, bank wil draw cards until scoring 17 or more
+            while (p_casino.score<17): # by Casino rules, Casino will draw cards until scoring 17 or more
                 p_casino.draw_card(c_deck)
             if (p_casino.score>21):
-                p_human.gain=1 # casino loses
+                p_human.gain=1 # Casino lost
             elif (p_human.score>p_casino.score):
-                p_human.gain=1 # player overscores casino
-            elif (p_human.score==p_casino.score): # both have 21 but player has no blackjack
+                p_human.gain=1 # Player overscores Casino
+            elif (p_human.score==p_casino.score): # both have 21 but Player has no blackjack
                 if not p_casino.blackjack:
                     p_human.gain=0 # deuce only if Casino has no blackjack either
                     
-    
     mongo.add_game(START_TIME, i, THRESHOLD, p_human, p_casino)
     p_human.reset()
     p_casino.reset()
