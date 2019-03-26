7e83f474c2bfcbfc481ca1fd6a927da592911b47
Maxime Beugnet <maxime.beugnet@gmail.com>
2019-03-26 20:12:18 +0100

    add README + remove warnings + code cleanup + code review

--- README.md
+++ README.md
@@ -1,8 +1,93 @@
-# mongodb-blackjack
-blackjack games simulator with data storage in MongoDB for game analysis
+# MongoDB Blackjack
 
-## contributors
+Blackjack games simulator with data storage in MongoDB for game analysis.
 
-Samuel Meister : for the original idea and designing the global algorithm
+# How to start
 
-Tatiana Meister : for support when rewriting code in OOP
+## MongoDB
+
+You will need to start a MongoDB instance to store the game results.
+
+### Start MongoDB locally
+
+If you want to work locally, you can install and run MongoDB yourself or you can use Docker.
+
+```
+docker run --rm -d -p 27017:27017 --name mongodb mongo:4.0.7
+```
+
+If you use this Docker command line or a MongoDB instance running locally, you will need to use the following URI to use your MongoDB instance.
+
+```
+mongodb://localhost
+```
+
+### Use MongoDB in the Cloud with MongoDB Atlas
+
+You can also create a free MongoDB cluster with [MongoDB Atlas](https://cloud.mongodb.com).
+
+If you want to connect to this instance, you will need to:
+
+- Create a free account on MongoDB Atlas.
+- Start a free tier cluster.
+- In the security tab at the top create a MongoDB user with read/write permissions at least.
+- In the IP whitelist in the security tab, add your current public IP address to the list of authorised IP address.
+
+Once this is done, you will be able to connect to this MongoDB cluster in the cloud from your computer using the provided URI when you click on the *connect* button.
+
+This should look like this one:
+
+```
+mongodb+srv://<username>:<password>@freesandbox-rirxn.mongodb.net/test?retryWrites=true
+```
+
+# Start the python program
+
+You have to use python 3 and you have to install the dependencies.
+
+```
+sudo pip3 install -r requirements.txt
+```
+
+Then you just have to execute the program with the correct MongoDB URI.
+
+For example:
+
+```
+python mongo-blackjack.py mongodb://localhost
+```
+
+Finally, you will find the documents in MongoDB:
+
+```js
+> use blackjack
+switched to db blackjack
+> db.games.findOne()
+{
+	"_id" : ObjectId("5c9a777597a19056bd168971"),
+	"TS" : ISODate("2019-03-26T20:03:17.013Z"),
+	"sample" : 0,
+	"threshold" : 15,
+	"playerHand" : [
+		3,
+		1,
+		6
+	],
+	"casinoHand" : [
+		8,
+		4,
+		6
+	],
+	"playerBlackjack" : false,
+	"casinoBlackjack" : false,
+	"playerScore" : 20,
+	"casinoScore" : 18,
+	"playerGain" : 1
+}
+```
+
+## Contributors
+
+- Samuel Meister : for the original idea and designing the global algorithm
+- Tatiana Meister : for support when rewriting code in OOP
+- Maxime Beugnet : for the code review and this awesome README :-).
--- classes.py
+++ classes.py
@@ -1,67 +1,70 @@
 import random
 import pymongo
 
-def last_eleven(hand,pos):
+
+def last_eleven(hand, pos):
     """Finds the first 11 in a list of number
 
     :param hand: A list, of numbers
     :param pos: An int, position to start searching from
     :rtype: An int, position where 11 was found, or 99 if not found
     """
-    i=pos
-    found=99
-    while i<=(len(hand)-1):
-        if hand[i]==11:
-            found=i
-            i=99
-        i+=1
-    return(found)
+    i = pos
+    found = 99
+    while i <= (len(hand) - 1):
+        if hand[i] == 11:
+            found = i
+            i = 99
+        i += 1
+    return found
+
 
 def is_blackjack(hand):
     """Returns true if the hand list contains [1,10] or [10,1]"""
-    if (hand[0]==1 and hand[1]==10) or (hand[1]==1 and hand[0]==10):
-        return(True)
-    else:
-        return(False)
+    return (hand[0] == 1 and hand[1] == 10) or (hand[1] == 1 and hand[0] == 10)
+
 
 class Deck:
     """Represents a full set of 52 cards desks to play with
 
     :param nb_decks: An int, number of card decks to use
     """
-    def __init__(self,nb_decks):
-        self.nb_decks=nb_decks
-        self.mid_deck = 26*nb_decks #used to re-shuffle deck and reset position
+
+    def __init__(self, nb_decks):
+        self.nb_decks = nb_decks
+        self.mid_deck = 26 * nb_decks  # used to re-shuffle deck and reset position
         self.shuffle_deck()
-        
+
     def shuffle_deck(self):
         """Creates a deck of nb_decks 52 cards games and shuffles it"""
         self.position_deck = 0
-        self.cards=list(range(1,11))*4*self.nb_decks+[10,10,10]*self.nb_decks*4
+        self.cards = list(range(1, 11)) * 4 * self.nb_decks + [10, 10, 10] * self.nb_decks * 4
         random.shuffle(self.cards)
-        
+
     def get_card(self):
         """Retrieves the card at position_deck position and increases position_deck
         if half the cards have been played, reshuffle and rest position_deck"""
         card = self.cards[self.position_deck]
-        self.position_deck+=1
-        if (self.position_deck>self.mid_deck):
+        self.position_deck += 1
+        if self.position_deck > self.mid_deck:
             self.shuffle_deck()
-        return(card)
+        return card
+
 
 class Player:
     """Represents a player drawing cards - human or casino"""
+
     def __init__(self):
         self.reset()
 
     def reset(self):
         """Resets the player hand for each new game"""
-        self.hand=[]
-        self.blackjack=False
-        self.score=0
-        self.gain=-1
+        self.hand = []
+        self.blackjack = False
+        self.score = 0
+        self.gain = -1
 
-    def draw_card(self,deck):
+    def draw_card(self, deck):
         """Draws a card from the deck and calculate player's hand score"""
         self.hand.append(deck.get_card())
         self.score_hand()
@@ -72,45 +75,47 @@ class Player:
         Unless a blackjack is detected, scoring is made by counting all aces as 11,
         then switch them back to 1 one after the other until score goes below 22
         """
-        if len(self.hand)==2:
-            self.blackjack=is_blackjack(self.hand)
-            hand=self.hand
+        if len(self.hand) == 2:
+            self.blackjack = is_blackjack(self.hand)
+            hand = self.hand
         else:
-            score=0
-            pos=0
+            score = 0
+            pos = 0
             # let's convert all 1 in 11
-            hand = [11 if x==1 else x for x in self.hand]
+            hand = [11 if x == 1 else x for x in self.hand]
             # compute score and switch back aces to 1 until score<=21
-            while (sum(hand)>21 and pos<99):
-            # if over 21 then change last 11 for 1
-                pos=last_eleven(hand,pos)
-                if pos<99:
-                    hand[pos]=1
-        self.score=sum(hand)
+            while sum(hand) > 21 and pos < 99:
+                # if over 21 then change last 11 for 1
+                pos = last_eleven(hand, pos)
+                if pos < 99:
+                    hand[pos] = 1
+        self.score = sum(hand)
 
     def add_gain(self, value):
-        self.gain+=value
+        self.gain += value
+
 
 class DAO:
     """Represents MongoDB blackjack.games collection to write each game into
 
     :param uri: A string, MongoDB URI to connect to
     """
-    def __init__(self,uri):
+
+    def __init__(self, uri):
         connection = pymongo.MongoClient(uri)
         self.db = connection.blackjack
         self.games = self.db.games
-        
-    def add_game(self,start_time,sample,threshold,p_human,p_casino):
-        game =  { "TS" : start_time,
-                    "sample" : sample,
-                    "threshold" : threshold,
-                    "playerHand" : p_human.hand,
-                    "casinoHand" : p_casino.hand,
-                    "playerBlackjack" : p_human.blackjack,
-                    "casinoBlackjack" : p_casino.blackjack,
-                    "playerScore" : p_human.score,
-                    "casinoScore" : p_casino.score,
-                    "playerGain" : p_human.gain }
-                
+
+    def add_game(self, start_time, sample, threshold, p_human, p_casino):
+        game = {"TS": start_time,
+                "sample": sample,
+                "threshold": threshold,
+                "playerHand": p_human.hand,
+                "casinoHand": p_casino.hand,
+                "playerBlackjack": p_human.blackjack,
+                "casinoBlackjack": p_casino.blackjack,
+                "playerScore": p_human.score,
+                "casinoScore": p_casino.score,
+                "playerGain": p_human.gain}
+
         self.games.insert_one(game)
